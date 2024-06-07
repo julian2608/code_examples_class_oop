0c396091722cdcfd936938eff6ae43c3c6ba9b93
julian2608 <julianmpn@gmail.com>
2024-06-06 22:01:50 -0500

    Add example OOP

---
+++ ejemplo_POO/Operacion.java
@@ -0,0 +1,13 @@
+package ejemplo_POO;
+
+abstract class Operacion {
+    protected double numero1;
+    protected double numero2;
+
+    public Operacion(double numero1, double numero2) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+    }
+
+    public abstract double calcular();
+}
