8160f9bea42fce9d6190a02c1233acd844bb0e7c
julian2608 <julianmpn@gmail.com>
2024-06-18 16:39:39 -0500

    Add example constructor with parameters

---
+++ clase_POO/ejemplos_con_constructor/Carro.java
@@ -0,0 +1,69 @@
+package clase_POO.ejemplos_con_constructor;
+
+public class Carro {
+
+    private String marca;
+    private String color;
+    private int modelo;
+    private int cilindraje;
+    
+    // Constructor sin parametros ó vacio 
+    public Carro () {}
+
+    // Constructor con parametros
+    public Carro(String marca, String color, int modelo, int cilindraje) {
+        this.marca = marca;
+        this.color = color;
+        this.modelo = modelo;
+        this.cilindraje = cilindraje;
+    }
+
+
+    // Métodos de comportamiento
+    public void encender() {
+        System.out.println("Motor encendido");
+    }
+
+    public void apagar() {
+        System.out.println("Motor apagado");
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Marca: " + marca + ", Modelo: " + modelo + ", Color: " + color + ", Cilindraje: " + cilindraje);
+    }
+
+    // Métodos accesores getters y setters
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getModelo() {
+        return this.modelo;
+    }
+
+    public void setModelo(int modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getCilindraje() {
+        return this.cilindraje;
+    }
+
+    public void setCilindraje(int cilindraje) {
+        this.cilindraje = cilindraje;
+    }
+
+
+}
